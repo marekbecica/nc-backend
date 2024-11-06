c89ccaeb660c45079c1121f84074c3c032596e95
Marek <marekbecica@gmail.com>
2024-11-06 15:29:01 +0100

    added types for user profile and DTO

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,6 +1,22 @@
+import { Request } from 'express';
+
 export interface AuthRequest extends Request {
-    user?: {
+    user: {
       uid: string;
       phoneNumber: string;
     };
+    body: UpdateProfileDTO;
+}
+
+export interface UserProfile {
+    phoneNumber: string;
+    name: string;
+    email: string;
+    updatedAt: Date;
+    createdAt: Date;
+}
+  
+export interface UpdateProfileDTO {
+    name: string;
+    email: string;
 }
